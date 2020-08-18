4cb6bc14cb6e28c07632ab2f05ba791777d64e15
Russell Morgan <rhmorgan13@comcast.net>
2020-08-18 17:16:45 -0500

    thru 22.3

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
   const [, dispatch] = useStoreContext();
 
-const removeFromCart = item => {
-  dispatch({
-    type: REMOVE_FROM_CART,
-    _id: item._id
-  });
-};
+  const removeFromCart = item => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id
+    });
+    idbPromise('cart', 'delete', { ...item });
+  };
 
 const onChange = (e) => {
   const value = e.target.value;
@@ -20,14 +22,18 @@ const onChange = (e) => {
       type: REMOVE_FROM_CART,
       _id: item._id
     });
+  
+    idbPromise('cart', 'delete', { ...item });
   } else {
     dispatch({
       type: UPDATE_CART_QUANTITY,
       _id: item._id,
       purchaseQuantity: parseInt(value)
     });
+  
+    idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
   }
-};
+}
 
   return (
     <div className="flex-row">
