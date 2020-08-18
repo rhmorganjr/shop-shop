9e2501ecb71e38d87d13d3dd5a1a69903fbfd57b
Russ Morgan <russmorgan@Russs-Mac-mini.lan1>
2020-08-18 18:23:38 -0500

    working with Stripe

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
@@ -50,6 +51,10 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Not logged in');
+    },
+    checkout: async (parent, args, context) => {
+      const order = new Order({ products: args.products });
+      const { products } = await order.populate('products').execPopulate();
     }
   },
   Mutation: {
